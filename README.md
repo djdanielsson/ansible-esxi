5cb9db0f6b1af47d6c7f1c5842691034f74faf6d
David Danielsson <david.danielsson@redhat.com>
2023-02-27 21:13:21 -0600

    finally got dynamic inventory plugin working

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# collection_template
-You can build a new repository for an Ansible Collection using this template by following [Creating a repository from a template](https://help.github.com/en/github/creating-cloning-and-archiving-repositories/creating-a-repository-from-a-template). This README.md contains recommended headings for your collection README.md, with comments describing what each section should contain. Once you have created your collection repository, delete this paragraph and the title above it from your README.md.
-
-# Foo Collection for Ansible
+# ESXi Collection for Ansible
 <!-- Add CI and code coverage badges here. Samples included below. -->
-[![CI](https://github.com/ansible-collections/REPONAMEHERE/workflows/CI/badge.svg?event=push)](https://github.com/ansible-collections/REPONAMEHERE/actions) [![Codecov](https://img.shields.io/codecov/c/github/ansible-collections/REPONAMEHERE)](https://codecov.io/gh/ansible-collections/REPONAMEHERE)
+[![CI](https://github.com/djdanielsson/esxi/workflows/CI/badge.svg?event=push)](https://github.com/djdanielsson/esxi/actions) [![Codecov](https://img.shields.io/codecov/c/github/djdanielsson/esxi)](https://codecov.io/gh/djdanielsson/esxi)
 
 <!-- Describe the collection and why a user would want to use it. What does the collection do? -->
 
@@ -107,7 +104,7 @@ See [Ansible Using collections](https://docs.ansible.com/ansible/devel/user_guid
 
 ## Release notes
 
-See the [changelog](https://github.com/ansible-collections/REPONAMEHERE/tree/main/CHANGELOG.rst).
+See the [changelog](https://github.com/djdanielsson/esxi/tree/main/CHANGELOG.rst).
 
 ## Roadmap
 
@@ -117,18 +114,18 @@ See the [changelog](https://github.com/ansible-collections/REPONAMEHERE/tree/mai
 
 <!-- List out where the user can find additional information, such as working group meeting times, slack/IRC channels, or documentation for the product this collection automates. At a minimum, link to: -->
 
-- [Ansible Collection overview](https://github.com/ansible-collections/overview)
+- [Ansible Collection overview](https://github.com/djdanielsson/overview)
 - [Ansible User guide](https://docs.ansible.com/ansible/devel/user_guide/index.html)
 - [Ansible Developer guide](https://docs.ansible.com/ansible/devel/dev_guide/index.html)
-- [Ansible Collections Checklist](https://github.com/ansible-collections/overview/blob/main/collection_requirements.rst)
+- [Ansible Collections Checklist](https://github.com/djdanielsson/overview/blob/main/collection_requirements.rst)
 - [Ansible Community code of conduct](https://docs.ansible.com/ansible/devel/community/code_of_conduct.html)
 - [The Bullhorn (the Ansible Contributor newsletter)](https://us19.campaign-archive.com/home/?u=56d874e027110e35dea0e03c1&id=d6635f5420)
-- [News for Maintainers](https://github.com/ansible-collections/news-for-maintainers)
+- [News for Maintainers](https://github.com/djdanielsson/news-for-maintainers)
 
 ## Licensing
 
 <!-- Include the appropriate license information here and a pointer to the full licensing details. If the collection contains modules migrated from the ansible/ansible repo, you must use the same license that existed in the ansible/ansible repo. See the GNU license example below. -->
 
-GNU General Public License v3.0 or later.
+MIT License
 
-See [LICENSE](https://www.gnu.org/licenses/gpl-3.0.txt) to see the full text.
+See [LICENSE](LICENSE) to see the full text.
---
+++ plugins/inventory/inventory.py
@@ -0,0 +1,110 @@
+from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
+from ansible.errors import AnsibleError
+from paramiko import SSHClient
+import paramiko
+import re
+
+DOCUMENTATION = '''
+  name: community.esxi.inventory
+  plugin_type: inventory
+  short_description: Generate inventory from ESXi
+  description: Generate inventory from powered on VM's running on ESXi
+  extends_documentation_fragment:
+    - constructed
+  options:
+    hostname:
+      description: The ESXi hypervisor's FQDN/IP
+      required: true
+      type: string
+      default: none
+    username:
+      description: Username to login to ESXi CLI
+      required: true
+      type: string
+      default: none
+    password:
+      description: Password for provided user
+      required: true
+      type: string
+      default: none
+'''
+
+EXAMPLES = '''
+# Minimal example
+plugin: community.esxi.inventory
+hostname: 'hypervisor.example.com'
+username: 'root'
+password: 'Password'
+'''
+
+def _populate(self, params):
+  client = SSHClient()
+  client.load_system_host_keys()
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  try:
+    client.connect(params['hostname'], username=params['username'], password=params['password'] )
+  except:
+    print("connection issue")
+
+  try:
+    stdin_, stdout_, stderr_= client.exec_command('vim-cmd vmsvc/getallvms | grep -v Vmid | awk \'{ print $1 }\'')
+    ids = stdout_.read().decode().strip()
+    _stderr = stderr_.read().decode()
+  except:
+    ignoreError = True
+    # print("exec command issue %s " % ids)
+    # print("stderr: %s" % _stderr)
+
+  # print(ids)
+  for id in ids.splitlines():
+    # print(id)
+    _command = 'vim-cmd vmsvc/get.guest ' + id
+    try:
+      _stdin, _stdout, _stderr= client.exec_command(_command)
+      vminfo = _stdout.read().decode()
+    except:
+      print("exec command on %s command" % _command)
+      print("exec command issue %s " % vminfo)
+
+    #print(vminfo)
+    try:
+      hostname    = (re.search('hostName\s=\s\"(.*)\",', vminfo)).group(1)
+      ipaddress   = (re.search('ipAddress\s=\s\"(([\d]{1,3}.){4})\",', vminfo)).group(1)
+      guestfamily = (re.search('guestFamily(.*)\s=\s\"(.*)\",', vminfo)).group(2)
+      guestid     = (re.search('guestId(.*)\s=\s\"(.*)\",', vminfo)).group(2)
+      geststate   = (re.search('guestState\s=\s\"(.*)\",', vminfo)).group(1)
+    except:
+      ignoreError = True
+
+    self.inventory.add_host(hostname)
+    self.inventory.set_variable(hostname, 'ansible_host', ipaddress)
+    self.inventory.set_variable(hostname, 'guest_family', guestfamily)
+    self.inventory.set_variable(hostname, 'guest_name', guestid)
+    self.inventory.set_variable(hostname, 'vm_state', geststate)
+
+  client.close()
+
+class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
+  NAME = 'community.esxi.inventory'
+
+  def verify_file(self, path):
+    # return true/false if this is possibly a valid file for this plugin to consume
+    valid = False
+    if super(InventoryModule, self).verify_file(path):
+    # base class verifies that file exists and is readable by current user
+      if path.endswith(('esxi.yaml', 'esxi.yml', 'esxi_inventory.yaml', 'esxi_inventory.yml')):
+        valid = True
+    return valid
+
+  def parse(self, inventory, loader, path, cache = True): #Return dynamic inventory from source
+    super(InventoryModule, self).parse(inventory, loader, path, cache)
+
+    config = self._read_config_data(path)
+    self._consume_options(config)
+
+    params = {}
+    params['hostname'] = self.get_option('hostname')
+    params['username'] = self.get_option('username')
+    params['password'] = self.get_option('password')
+
+    results = _populate(self, params)
